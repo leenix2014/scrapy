ef8aca4fdae11e55dacb3031330e0d359323dad0
李全林 <liquanlin@stevegame.cn>
2018-12-19 03:08:03 +0800

    获取所有pdf

---
+++ scrap.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func main() {
+	urls := []string{
+		"http://www.cmbc.com.cn/jrms/msdt/yjbg/index.htm",
+		"https://www.hkma.gov.hk/gb_chi/publications-and-research/annual-report/2017.shtml",
+		"https://www.bochk.com/m/sc/investment/econanalysis/bocecon.html",
+	}
+
+	for _, url := range urls {
+		getAllPdf(url)
+	}
+}
+
+func getAllPdf(root string) {
+	url, _ := url.Parse(root)
+	// Request the HTML page.
+	resp, err := http.Get(root)
+	if err != nil {
+		//log.Fatal(err)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		//log.Fatalf("status code error: %d %s", resp.StatusCode, resp.Status)
+		return
+	}
+
+	//body, _ := ioutil.ReadAll(resp.Body)
+	//fmt.Println(string(body))
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pdfs := make(map[string]bool)
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		href, exists := s.Attr("href")
+		if exists && strings.Contains(href, ".pdf") {
+			if strings.HasPrefix(href, "/") {
+				pdfs[url.Scheme+"://"+url.Host+href] = false
+			} else {
+				pdfs[url.String()+href] = false
+			}
+		}
+	})
+	for pdf, _ := range pdfs {
+		fmt.Println(pdf)
+	}
+}
